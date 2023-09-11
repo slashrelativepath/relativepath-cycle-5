5bc6b8c984b7e789c2e902f0e36d498cff767f6f
Chris Forti <christopher.forti.79@gmail.com>
2023-09-11 18:59:29 -0400

    Add brew for mac.

--- webserver-darwin.sh
+++ webserver-darwin.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/zsh
 
-echo "git should bee installed"
+echo "brew should be installed"
+if ( which brew )
+then
+  echo "brew already installed"
+else
+  echo "installing brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "git should be installed"
+if ( which git )
+then 
+  echo "git already installed"
+else 
+  echo "installing git"
+  brew install git
+fi
 
 echo "nano should be installed"
+if ( which nano )
+then
+  echo "nano already installed"
+else
+  echo "installing nano"
+  brew install nano 
+fi
+
