79f63d80857d844510c7c08793af8bba4796724b
HarmonyMorse <harmonymorse@icloud.com>
2023-09-18 15:47:06 -0700

    Fix chocolatey install command.

--- webserver.ps1
+++ webserver.ps1
@@ -4,5 +4,4 @@
 
 # install chocolatey
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManag$
-
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
