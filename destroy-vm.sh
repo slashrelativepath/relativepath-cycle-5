9499dd77c4af50c8f0736e54b6c868408ab7e42a
Christopher Forti <christopher.forti.79@gmail.com>
2023-09-20 17:29:10 -0400

    Update destroy-vm.sh

--- destroy-vm.sh
+++ destroy-vm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # delete and purge relativepath VM
-if ( multipass list | grep relativepath )
+if ( ! multipass list | grep relativepath )
 then
+  echo "no relativepath VM found"
+else 
   multipass delete --purge relativepath
   echo "relativepath VM has been deleted and purged"
-else
-  echo "no relativepath VM found"
 fi
