5aded086ac5fb8c47ab7247e8565364f9ea57caa
Seyamack Afzali <seyamack@gmail.com>
2023-09-24 08:45:55 -0500

    updated ubuntu/debian shell script to configure the desired state of our machine to support multipass. Tested on an ubuntu VM

---
+++ webserver-ubuntu.sh
@@ -0,0 +1,50 @@
+
+echo "checking if Nano is installed"
+if ( which nano )
+then
+  echo "Nano already installed"
+else
+  echo "Installing Nano"
+  apt install -y nano
+fi
+
+echo "Checking if Git is installed"
+if ( which git )
+then 
+  echo "Git is installed"
+else
+  echo "Installing Git"
+  apt install -y git
+fi
+
+
+echo "Checking if Multipass is installed" 
+if ( multipass version ) 
+then
+  echo "Multipass is installed" 
+else 
+  echo "Installing Multipass" 
+  # the following should not be necessary on recent versions of Ubuntu
+  # checking for snapd anyway
+  if ( which snap )
+  then 
+    echo "snapd installed"
+  else
+    echo "installing snapd"
+    sudo apt update && sudo apt install -y snapd
+    # restarts shell
+    exec bash
+  fi
+    # systemctl status snapd.service
+    echo "installing multipass"
+    sudo snap install multipass
+
+# the following breaks this script in a bare ubuntu container
+#   git clone https://aur.archlinux.org/snapd.git
+#   cd snapd
+#   makepkg -si
+#   cd ..
+#   sudo systemctl enable --now snapd.socket
+#   sudo ln -s /var/lib/snapd /snap
+#   sudo snap install multipass
+fi
