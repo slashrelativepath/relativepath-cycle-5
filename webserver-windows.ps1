571709cae3911a45827186b39428592fd5ddb737
Seyamack Afzali <seyamack@gmail.com>
2023-09-23 19:52:37 -0500

    removed comment regarding running this script as administrator. Instructions within the README should direct the user to set an execution policy prior to this script being run.

--- webserver-windows.ps1
+++ webserver-windows.ps1
@@ -6,7 +6,6 @@ if (Get-Command -Name choco.exe -ErrorAction SilentlyContinue) {
 }
 else {
     Write-Output "Installing Chocolatey"
-    # ensure that you are using an administrative shell - a non-admin installation exists
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
