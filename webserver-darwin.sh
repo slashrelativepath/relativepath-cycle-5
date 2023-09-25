76a560497361417769a4d6d55f1c76a98e4cbe98
Bianca <xolotl.tonatiuh@gmail.com>
2023-09-25 15:37:25 -0700

    Readd darwin script.

---
+++ webserver-darwin.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/zsh
+
+echo "brew should be installed"
+if ( which brew )
+then
+  echo "brew already installed"
+else
+  echo "installing brew"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+echo "git should be installed"
+if ( which git )
+then 
+  echo "git already installed"
+else 
+  echo "installing git"
+  brew install git
+fi
+echo "nano should be installed"
+if ( which nano )
+then
+  echo "nano already installed"
+else
+  echo "installing nano"
+  brew install nano 
+fi
+ echo "installing multipass on $(uname)"
+  if [ "$(uname)" = "Darwin" ]
+  echo "multipass already installed on $(uname)"
+else
+  brew install --cask multipass
+  echo "installing multipass on $(uname)"
+fi
+if ( multipass info relativepath | grep Running )
+then 
+  echo "relativepath vm is running"
+else 
+  echo "launching a ubuntu vm named relativepath"
+  multipass launch --name relativepath
+fi
